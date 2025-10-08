7c821ff9e81343bf08c1acef992fe47a5fac9e62
maxence-glt <maxencegilloteaux2023@gmail.com>
2025-10-08 18:05:04 -0500

    initial ray tracer

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+
+double hit_sphere(const point3 &center, double radius, const ray &r) {
+    vec3 oc = center - r.orig;
+    auto a = dot(r.dir, r.dir);
+    auto b = -2.0 * dot(r.dir, oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    
+    if (discriminant < 0) return -1.0;
+    return (-b - std::sqrt(discriminant)) / (2.0*a);
+}
+
+color ray_color(const ray &r) {
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+
+    vec3 unit_direction = unit_vector(r.dir);
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
+int main() {
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+    int image_height = std::max(int(image_width / aspect_ratio), 1);
+
+    // camera
+    auto focal_length = 0.5;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // calculate the vectors across hoizontal/vertical viewport edges
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // calculate the horizontal/vertical delta vectors
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // calculate upper left pixel
+    auto viewport_upper_left = camera_center 
+                             - vec3(0, 0, focal_length)
+                             - viewport_u/2
+                             - viewport_v/2;
+
+    auto pixel00_loc = viewport_upper_left + 0.5*(pixel_delta_u + pixel_delta_v);
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            auto pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+
+    return 0;
+}
